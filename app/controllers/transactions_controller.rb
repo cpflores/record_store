41644106d64b6a9fd3fc4c77a436b7d4ca452f98
Chadwick Flores <chadwickflores@gmail.com>
2015-02-03 22:11:26 -0600

    fix transaction create and pickup

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,17 +8,18 @@ class TransactionsController < ApplicationController
         amount: record.price,
         currency: "usd", 
         card: token, 
-        descrtiption: current_user.email)
+        description: current_user.email)
       @sale = record.sales.create!(
         buyer_email: current_user.email)
       redirect_to pickup_url(guid: @sale.guid)
-    rescue Stripe::CardError => each e
+    rescue Stripe::CardError => e
       @error = e
       redirect_to record_path(record), notice: @error
+    end
   end
 
   def pickup
-    @sale = Sale.find_by(guid: params[:guid])
+    @sale = Sale.find_by!(guid: params[:guid])
     @record = @sale.record
   end
 end
